8714fc4bdd59132739edd465696ae0200f4e2557
aakash4dev <aakash4dev.crypto@gmail.com>
2024-01-27 18:07:55 +0530

    env and constants updated

--- common/constants.go
+++ common/constants.go
@@ -2,8 +2,8 @@ package common
 
 const BatchSize = 25
 const BlockDelay = 5
-const ExecutionClientRPC = "http://192.168.1.25:8545/"
-const SettlementClientRPC = "http://192.168.1.25:8080"
+const ExecutionClientRPC = "http://0.0.0.0:8545/"
+const SettlementClientRPC = "http://0.0.0.0:8080"
 const KeyringDirectory = "./account/keys"
 // var DaClientRPC = os.Getenv("DA_CLIENT_RPC")
-const DaClientRPC = "http://192.168.1.25:5050/celestia"
+const DaClientRPC = "http://0.0.0.0:5050/celestia"
